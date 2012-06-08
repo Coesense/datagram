1f41e6f48d90c14e46f4e930d5d90b428fa011d8
Jonathan Levy <jimode.levy@gmail.com>
2012-06-08 16:04:04 +0200

    count api

---
+++ app-api/app-count.php
@@ -0,0 +1,44 @@
+<?php
+//header('Content-type: application/json');
+
+class data {
+
+	public function __construct(){}
+	
+	/**
+	* Data from teleportd
+	* @JSON
+	*/
+	public function getData($query_tab = array()){
+		$src_tab = array('instagram','twimg','yfrog');
+		for ($i=0; $i<3; $i++) {
+			$c = curl_init();
+			curl_setopt($c, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7');
+			curl_setopt($c, CURLOPT_URL, 'http://api.teleportd.com/search?user_key=c0a5082668f603d6368498b314bdc021&str='.$query_tab['hash'].'src:'.$src_tab[$i].'&loc='.$query_tab['loc']);
+			curl_setopt($c, CURLOPT_TIMEOUT,15); 
+			curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($c, CURLOPT_HEADER, false);
+			$transit_tab = json_decode(curl_exec($c));
+			$main_tab[$src_tab[$i]] = $transit_tab->total;
+		}
+		return json_encode($main_tab);
+	}
+	
+	/**
+	* Query cleaned
+	* @string
+	*/
+	public function treatQuery($hash,$loc){
+		return $query_tab = array(
+			"hash" => $hash,
+			"loc"  => $loc
+		);
+	}
+
+}
+
+
+$api_data = new data();
+echo $api_data->getData($api_data->treatQuery(preg_replace('#\ #', '%20', $_POST['hash']),$_POST['loc']));
+
+?>
