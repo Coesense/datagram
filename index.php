849ea38c00ce653e6a25a60968b88353ad7ab442
LoicUV <Loic Huve>
2012-06-21 17:25:03 +0200

    added g.analytics

--- index.php
+++ index.php
@@ -19,6 +19,17 @@
 <script src="app-js/app-raphael.js" type="application/javascript"></script>
 <script src="app-js/app-geo.js" type="application/javascript"></script>
 <script src="app-js/app-main.js" type="application/javascript"></script>
+<script type="application/javascript">
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-32846901-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+</script>
 
 <!--TITLE-->
 <title>DATA'GRAM - Search engine for local amateur photos!</title>
